38728d81066d03d692226f69d49342fbd6e9d975
Vadim <vadopolski@gmail.com>
2021-12-09 09:53:44 +0300

    Changes

--- src/main/scala/sber2/popkov/RddMain.scala
+++ src/main/scala/sber2/popkov/RddMain.scala
@@ -1,6 +1,9 @@
 package sber2.popkov
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
+import scala.collection.immutable
 
 object RddMain extends App{
   val spark = SparkSession.builder()
@@ -11,10 +14,10 @@ object RddMain extends App{
   val sc = spark.sparkContext
 
   // read from file
-  val stateNamesCSV: RDD[String] = sc.textFile("src/main/resources/statenames.csv")
+  val taxiZonesCSV: RDD[String] = sc.textFile("src/main/resources/taxi_zones.csv")
 
   // split / clean data
-  val headerAndRows: RDD[List[String]] = stateNamesCSV.map(line => line.split(",").toList.map(x => x.trim))
+  val headerAndRows: RDD[List[String]] = taxiZonesCSV.map(line => line.split(",").toList.map(x => x.trim))
 
   // get header
   val header: immutable.Seq[String] = headerAndRows.first().toList
@@ -22,6 +25,7 @@ object RddMain extends App{
   // filter out header (eh. just check if the first val matches the first header name)
   val data = headerAndRows.filter(x => x.head != header.head)
 
-  println("Test")
+  val rowsample = headerAndRows.sample(false,0.01).toString()
+  println(rowsample)
 
 }
